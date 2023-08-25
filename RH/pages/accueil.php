6439157ea844922539d97539a046821aa6ae5b53
Maha yaddas <mahayaddas1@gmail.com>
2023-08-25 16:12:58 +0100

    first commit

---
+++ RH/pages/accueil.php
@@ -0,0 +1,182 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+   <head>
+      <!--links para o funcionamento da pág-->
+      <meta charset="utf-8">
+   
+
+      <title>RJRH</title>
+
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+      <link rel="stylesheet" href="estilos/estilopag/sty.css">
+      <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+      <link rel="stylesheet" type="text/css" href="sty.css">
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
+
+   </head>
+
+<body>
+
+       <!--inicio da nav-bar-->
+     <nav class="navbar navbar-expand-lg navbar-light bg-dark py-3 shadow-sm  fixed-top"> <a href="#" class="navbar-brand font-weight-bold d-block d-lg-none"></a>
+  
+    <div id="navbarContent" class="collapse navbar-collapse">
+        <ul class="navbar-nav mx-auto">
+         <a class="navbar-brand" href="#" style="margin-left: -500px;margin-right: 400px ">DJ<span style="color:yellow">RH</span></a>
+          
+            <!--itens da nav-->
+            <li class="nav-item dropdown megamenu"><a id="megamneu" href="test.php" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link  font-weight-bold text-uppercase disabled">Accueil</a>
+            <li class="nav-item"><a href="candidature.php" class="nav-link font-weight-bold text-uppercase">Candidater</a></li>
+             <form class="d-flex" style="background-color: transparent;">
+        <a class="btn btn-outline-warning" type="submit" href="inscription.php">S'inscrire</a>&nbsp;
+        <a class="btn btn-outline-warning" type="submit" href="connexion.php">Se connecter</a>
+      </form>
+
+        </ul>
+    </div>
+</nav>
+
+     <!--carrosel com informações sobre o app-->
+      <div id="home" class="slider">
+         <div id="main_slider" class="carousel slide" data-ride="carousel">
+          
+            <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <img class="d-block w-100" src="../images/acc.jpg" style="height: 700px" alt="slider_img">
+                  <div class="ovarlay_slide_cont">
+                     <h2>Bienvenue,</h2>
+                     <h4>decouvrez avec nous l'art de l'RH</h4>
+                     <p> l'ultime intermediaire qui se fait entre les employés, <br> candidats et Entreprise ... &#9786;<br/></p>
+                     <a href="candidature.php"><button class="button button2">Candidater</button></a>
+                  </div>
+               </div>
+
+
+          
+
+       
+         </div>
+      </div>
+
+
+        <!--informação sobre aplicativo e função-->
+      <div id="about" class="layout_padding">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-5">
+                  
+                  <h3 style="text-transform: none !important">Espace candidat</h3><br>
+                  <h5> Vous etes a la recherche d'un emploi ? <br/>
+    N'hesitez surtout pas à postuler auprès de notre <br>entreprise. <br/>
+    suffit juste de suivre le lien indiqué ici  &#8681;.
+    <br/>  </h5>
+     <a href="candidature.php"><button class="button button2 bg-warning">Candidater</button></a>
+               </div>
+               <div class="col-md-6 offset-md-1">
+                  <div class="full text_align_center">
+                     <img class="img-responsive"src="../images/logoo.jpg" alt="#" />
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+
+
+      
+
+       
+           
+      
+
+              <!--informação adicional-->
+      <div id="wcs" class=" layout_padding">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 text_align_center">
+                  <h4>N'hesitez pas a nous contacter ! </h4><br>
+                  
+               </div>
+               <div class="col-md-5">
+               </div>
+            </div>
+
+            <div class="row">
+               <div class="col-md-11">
+                  <div class="full testimonial_blog">
+                     <p>Besoin de plus d'information ?</p>
+                     <p>vous trouverez en dessous les informations necessaires pour pouvoir nous contacter.</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      
+            <!--rodape-->
+  <section id="footer"  style="background-color: #203154;"><br>
+    <div class="container">
+      <div class="row text-center text-xs-center text-sm-left text-md-left">
+        <div class="col-xs-12 col-sm-4 col-md-4">
+          <h5 style="color: white">DJRH</h5>
+     
+        </div>
+        <div class="col-xs-12 col-sm-4 col-md-4">
+          <h5 style="color: white">Liens</h5>
+          <ul class="list-unstyled quick-links">
+            <li><a href=""><i class="fa fa-angle-double-right"></i>Accueil</a></li>
+            <li><a href="candidature.php"><i class="fa fa-angle-double-right"></i>Candidature</a></li>
+            
+          </ul>
+        </div>
+        <div class="col-xs-12 col-sm-4 col-md-4">
+           <h5 style="color: white">Adresse</h5>
+         
+            
+                    <address style="color: white">
+                  2,Rue Roudani,Maarif<br>
+                  Quartier des warior,Casablanca<br>
+                  MAROC<br>
+                  <i class="fa fa-phone fa-lg"></i>: +212 0601023093<br>
+                      <i class="fa fa-fax fa-lg"></i>: +212 0607010405<br>
+                      <i class="fa fa-envelope fa-lg"></i>: 
+                      <a href="mailto:DJRH@gmail.com"> DJRH@gmail.com</a>
+               </address>
+           
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+          <ul class="list-unstyled list-inline social text-center">
+            <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fa fa-google-plus"></i></a></li>
+            <li class="list-inline-item"><a href="#" target="_blank"><i class="fa fa-envelope"></i></a></li>
+          </ul>
+        </div>
+        <hr>
+      </div>  
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+        
+          <p class="h6">© All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">DJRH</a></p>
+        </div>
+        <hr>
+      </div>  
+    </div>
+  </section>
+   
+        <!--script para o funcionamento do carrosel-->
+      <script src="js/jquery-3.3.1.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+       
+
+      
+     
+   </body>
+</html>
